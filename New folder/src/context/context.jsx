07215e789f4735f0bb8df87df6c3065a2d4d8b5e
Satirtha Ghosal <138100530+Satirtha-Ghosal@users.noreply.github.com>
2025-04-25 11:42:42 +0530

    Update context.jsx

--- New folder/src/context/context.jsx
+++ New folder/src/context/context.jsx
@@ -4,8 +4,8 @@ const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
     const [user, setUser] = useState({
-        phone: '1000000004',
-        role: 'farmer',
+        phone: '',
+        role: '',
     });
 
     return (
